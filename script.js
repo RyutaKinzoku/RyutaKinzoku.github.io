236d47dd4726806bdffcdaf6fe779623796c0542
Kenneth Ibarra Vargas <66222501+RyutaKinzoku@users.noreply.github.com>
2024-08-06 21:09:18 -0600

    Change and add URLs to the projects

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 // Dynamic project loading
 const projects = [
-    { name: "Concasa's AI-Powered Real Estate Chatbot", description: "Erica, an advanced chatbot designed to assist potential homebuyers at Concasa. Leveraging Python, Flask, and cutting-edge AI technologies such as GPT-3 and GPT-4, Erica provides intelligent and informative responses to customer inquiries. The chatbot effectively integrates with multiple platforms through APIs, including Meta's Graph API, Gmail API, and HubSpot API. Employed Agile methodologies with Scrum and Jira to optimize development, planning, and quality assurance. You can try it messaging to this links https://wa.me/50688482272 or https://www.facebook.com/concasa?locale=es_LA" },
-    { name: "Game Jam+ Costa Rica 2022 - A Costa Rican Horror Story", description: "A 2D survival horror game as part of the Memento Factory team. Leveraging Unity and C#, helped create a suspenseful atmosphere based on Costa Rican folklore. Focused on puzzle-solving and resource management gameplay within a labyrinthine environment." },
+    { 
+        name: "Concasa's AI-Powered Real Estate Chatbot", 
+        description: "Erica, an advanced chatbot designed to assist potential homebuyers at Concasa. Leveraging Python, Flask, and cutting-edge AI technologies such as GPT-3 and GPT-4, Erica provides intelligent and informative responses to customer inquiries. The chatbot effectively integrates with multiple platforms through APIs, including Meta's Graph API, Gmail API, and HubSpot API. Employed Agile methodologies with Scrum and Jira to optimize development, planning, and quality assurance.",
+        links: [
+            {text: "Press Release", url: "https://www.larepublica.net/noticia/talento-tico-desarrolla-asistente-inmobiliaria-con-base-en-inteligencia-artificial"},
+            {text: "Live Demo WhatsApp", url: "https://wa.me/50688482272"},
+            {text: "Live Demo Facebook Messenger", url: "https://www.facebook.com/concasa?locale=es_LA"}
+        ]
+    },
+    { 
+        name: "Game Jam+ Costa Rica 2022 - A Costa Rican Horror Story", 
+        description: "A 2D survival horror game as part of the Memento Factory team. Leveraging Unity and C#, helped create a suspenseful atmosphere based on Costa Rican folklore. Focused on puzzle-solving and resource management gameplay within a labyrinthine environment.",
+        links: [
+            {text: "What is Game Jam+?", url: "https://www.cidev-cr.com/gamejamplus22/"},
+            {text: "Memento Factory Team", url: "https://www.youtube.com/watch?v=0S0HuY0y4SQ&t=118s"},
+            {text: "itch.io to download", url: "https://indonirth.itch.io/a-costa-rican-horror-story"}
+        ]
+    },
     
 ];
 
